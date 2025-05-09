e5b2a43b82c18e9df9553863268420d0ca536ed9
cyrusnderitu <cyrusnderitu.k@gmail.com>
2025-05-09 11:04:12 +0300

    links addition plus accordion

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const sliderTrack = document.getElementById("sliderTrack");
-  const prevBtn = document.getElementById("prevBtn");
-  const nextBtn = document.getElementById("nextBtn");
+const prevBtn = document.getElementById("prevBtn");
+const nextBtn = document.getElementById("nextBtn");
+const headers = document.querySelectorAll(".accordion-header");
 
   let currentIndex = 0;
   const cardWidth = 316; // includes margin (min-width + gap)
@@ -35,4 +36,25 @@ const sliderTrack = document.getElementById("sliderTrack");
     animation: {
       duration: 300
     }
+  });
+
+
+
+  headers.forEach(header => {
+    header.addEventListener("click", () => {
+      const body = header.nextElementSibling;
+      const isActive = header.classList.contains("active");
+
+      document.querySelectorAll(".accordion-body").forEach(p => p.style.display = "none");
+      document.querySelectorAll(".accordion-header").forEach(h => {
+        h.classList.remove("active");
+        h.querySelector("span").textContent = "+";
+      });
+
+      if (!isActive) {
+        body.style.display = "block";
+        header.classList.add("active");
+        header.querySelector("span").textContent = "–";
+      }
+    });
   });
